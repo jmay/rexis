be5d9283d00f21947627ef89460273e20127131f
Jason W. May <jmay@pobox.com>
2014-03-22 10:49:29 -0700

    foreman support

--- lib/rexis.rb
+++ lib/rexis.rb
@@ -3,9 +3,8 @@ require "dotenv"
 
 module Rexis
   def self.config
-    env||= ENV["APP_ENV"] || ENV["RACK_ENV"] || "development"
-    Dotenv.load ".#{env}.env", ".env"
-
+    env ||= ENV["APP_ENV"] || ENV["RACK_ENV"] || "development"
+    Dotenv.load ".env.#{env}", ".env"
     Sequel.connect(ENV["DATABASE_URL"])
   end
 end
